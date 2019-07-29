e0d098fdda928f94cf3267b940d6206f3eef0222
Magnus Frennberg <mfrennberg@hotmail.com>
2019-07-29 14:47:38 -0400

    images added

--- InsertRun.py
+++ InsertRun.py
@@ -2,8 +2,25 @@
     Insert Run function.
 '''
 
-def InsertRun(profile_id, ):
+def InsertRun(db_connector, list_of_arg):
+    '''
+        Order of arguments should be:
+        - user_id_param (integer)
+	    - profile_id_param (integer),
+	    - duration_param (integer),
+        - distance_in_miles_param (float),
+        - circle_id_param (integer),
+        - elevation_param (integer),
+        - location_id_param (integer),
+        - smart_integration_param (integer),
+        - scheduled_date_param DATETIME
+    '''
 
+    cursor = db_connector.cursor()
 
-
-
+    user_id_param = list_of_arg[0]
+    profile_id_param = list_of_arg[1]
+    duration_param = list_of_arg[2]
+    distance_in_miles_param = list_of_arg[3]
+    circle_id_param = list_of_arg[4]
+    

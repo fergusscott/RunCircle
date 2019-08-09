605878b3c764de35f965bcd4b549d730073b871b
Magnus Frennberg <mfrennberg@hotmail.com>
2019-08-09 14:13:49 -0400

    Cleaned up

--- PythonTestInterfaces/insert_run.py
+++ PythonTestInterfaces/insert_run.py
@@ -3,9 +3,17 @@
 '''
 
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import errorcode
 
-def InsertRun(db_connector, list_of_arg):
+# Globals to use in the function. Probably should not be stored here,
+# but for the purpose of the project, they can be here.
+USER = 'root'
+PASSWORD = 'runcircle123' 
+HOST = 'localhost'
+DATABASE = 'run_circle'
+
+
+def InsertRun(list_of_arg):
     '''
     Parameters: A list of arguments. Order of arguments should be:
         - user_id_param (integer)
@@ -23,18 +31,41 @@ def InsertRun(db_connector, list_of_arg):
     Does: Adds a run with the passed parameters to the DB.
     '''
 
-    cursor = db_connector.cursor()
+    # Set up dictionairy of connection data.
+    DBconfig = {
+    'user': USER,
+    'password': PASSWORD,
+    'host': HOST,
+    'database': DATABASE
+    }
+
+    try:
+        # Here I am "dereferencing" the dictionairy I made.
+        connection = mysql.connector.connect(**DBconfig)
+    # Handle errors.
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+        return
+    
+
+    cursor = connection.cursor()
 
     # Cause I know the args are correct, for now
     arg_tup = tuple(list_of_arg)
 
     try:
         cursor.callproc("InsertRun", arg_tup)
-        db_connector.commit()
+        connection.commit()
         cursor.close()
     except mysql.connector.Error as err:
         print("Something went crazy: {}".format(err))
 
     cursor.close()
+    connection.close()
     
         

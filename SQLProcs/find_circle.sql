7ec782f47b55d2df8d60e0296dba9fd8582cc73e
Magnus Frennberg <mfrennberg@hotmail.com>
2019-08-09 13:41:21 -0400

    Added a find circle function

---
+++ SQLProcs/find_circle.sql
@@ -0,0 +1,30 @@
+USE run_circle;
+DROP PROCEDURE IF EXISTS findRunCircle;
+
+# A function very similar to run_friend (read that first)
+# We determine a suitable circle in your area. 
+# It'll be based on average run duration and speed.
+
+DELIMITER // 
+CREATE PROCEDURE findRunCircle(
+	in profile_id_param INT
+)
+BEGIN
+    DECLARE profile_avg_duration TIME;
+    DECLARE profile_preferred_location INT;
+    
+    # Fill up the variables!
+    SELECT avg_duration INTO profile_avg_duration FROM profile WHERE profile_id = profile_id_param;
+	SELECT preferred_location INTO profile_preferred_location FROM profile WHERE profile_id = profile_id_param;
+    
+    SELECT 
+        avg_duration, 
+        location_id,
+        circle_id,
+        name
+	FROM circle
+    WHERE preferred_location = profile_preferred_location 
+		and (ABS(avg_duration - profile_avg_duration) < (profile_avg_duration * 0.05))
+        ;
+    
+END //
